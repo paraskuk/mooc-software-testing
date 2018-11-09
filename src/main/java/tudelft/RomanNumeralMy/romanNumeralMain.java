2f30d1bc5bf25215997458c4c0d549176a844482
Paraskevas <paraskevasp@gmail.com>
2018-11-09 19:28:28 +0000

    Completed the romsn numerals

---
+++ src/main/java/tudelft/RomanNumeralMy/romanNumeralMain.java
@@ -0,0 +1,14 @@
+package tudelft.RomanNumeralMy;
+import java.;
+public class romanNumeralMain {
+
+    public static void main(String[] args) {
+        RomanNumeral rn = new RomanNumeral();
+
+
+            System.out.println(rn.convert("IV"));
+            // this works
+            System.out.println(rn.convert("IVVBN"));
+
+        }
+    }
